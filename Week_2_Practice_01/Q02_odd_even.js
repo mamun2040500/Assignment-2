53c436247cd0846327d28c09db17d199cdf679ee
mamun2040500 <146740974+mamun2040500@users.noreply.github.com>
2025-01-27 01:54:12 +0600

    Add files via upload

---
+++ Week_2_Practice_01/Q02_odd_even.js
@@ -0,0 +1,17 @@
+
+const OddEven = (num) => {
+    if (num % 2 == 0) {
+        console.log('Even');
+    }
+    else {
+        console.log('Odd');
+    }
+};
+
+OddEven (22);
+OddEven (33);
+OddEven (44);
+OddEven (55);
+OddEven (66);
+OddEven (77);
+OddEven (88);
